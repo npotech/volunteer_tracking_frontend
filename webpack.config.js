9ec0b61aa9de541332623eefca9604afbdffd911
Jerel Miller <jerelmiller@gmail.com>
2019-03-23 10:54:50 -0700

    Add prettier and set history api fallback

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.jsx',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-      path: __dirname + '/dist',
-      publicPath: '/',
-      filename: 'index.js'
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title:'Volunteer Checkin',
-        inject:true,
-        template:'src/index.html',
-        filename: './index.html'
-      }),
-      new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-      contentBase: __dirname + '/dist',
-      compress: true,
-      port: 9000
-    }
-  };
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Volunteer Checkin',
+      inject: true,
+      template: 'src/index.html',
+      filename: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: __dirname + '/dist',
+    compress: true,
+    port: 9000,
+    historyApiFallback: true
+  }
+}
